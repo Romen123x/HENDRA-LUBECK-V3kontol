578bd7bdc2dcb5ea6b026ab124e9363a8a079a10
lonte123x <116000602+lonte123x@users.noreply.github.com>
2023-01-22 20:11:04 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/8f7e2b662ac8dc4f7f562.jpg`
-    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/628812743180 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/6281227607864 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/saep.02",
+                mediaUrl: "https://youtube.com/@hidayah-99",
                 mediaType: "VIDEO",
-                title: 'Donasi To NexEp ',
+                title: 'Donasi To HENDRA LUBECK🇮🇩 ',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
